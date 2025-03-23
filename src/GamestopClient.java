57e62c7886606ea906c5007848b01d66a7bb06e3
aysek <aysea.koyuncu1705@gmail.com>
2025-03-23 21:56:18 +0100

    client bearbeiten

--- src/GamestopClient.java
+++ src/GamestopClient.java
@@ -1,12 +1,34 @@
 import utils.Client;
+import javax.swing.JOptionPane;
 
 public class GamestopClient extends Client {
 
-    public GamestopClient(String pServerIP, int pServerPort){}
+    public GamestopClient(String pServerIP, int pServerPort){
+        super("127.0.0.1", 38);
+    }
 
-    public void requestGame (String gameName){}
+    public void requestGame (String gameName){
+        send("RequestedGame: " + gameName);
+    }
 
-    public void abmelden (){}
+    public void abmelden (){
+        send("ABMELDEN");
+    }
 
-    public void processMessage (String pMessage) {}
+    public void processMessage (String pMessage) {
+        JOptionPane.showMessageDialog(null,"Server sendet:\n" + pMessage);
+        if (pMessage.toUpperCase().startsWith("HERZLICH")){
+
+        } else if (pMessage.toUpperCase().startsWith("Gib")){
+            String h = "";
+            requestGame(h);
+        } else if (pMessage.toUpperCase().startsWith("Gib")){
+            String h = "";
+            requestGame(h);
+        } else if (pMessage.toUpperCase().startsWith("Gib")){
+            String h = "";
+            requestGame(h);
+        }
+        abmelden();
+    }
 }
--- src/GamestopServer.java
+++ src/GamestopServer.java
@@ -3,19 +3,44 @@ public class GamestopServer extends Server {
 
     private List<Game> ListOrders;
 
-    public GamestopServer(int pPortnummer, List<Game> ListOrders){}
+    public GamestopServer(int pPortnummer, List<Game> ListOrders){
+        super(38);
+        System.out.println("Server gestartet.");
+    }
 
-    public void processNewConnection (String pClientIP, int pClientPort){}
+    public void processNewConnection (String pClientIP, int pClientPort){
+        String beginn = "Herzlich Willkommen beim Gamestop. ";
+        send(pClientIP,pClientPort,beginn);
+    }
 
-    private void processNewMessage (String pClientIP, int pClientPort){}
+    //private void processNewMessage (String pClientIP, int pClientPort){}
 
-    public Game findGame (String pMessage){}
+    public Game findGame (String pMessage){
+        Game l = null;
+        ListOrders.toFirst();
+        while(ListOrders.hasAccess()){
+            if(ListOrders.getContent().equals(pMessage)){
+               l = ListOrders.getContent();
+               ListOrders.toLast();
+            }
+            ListOrders.next();
+        }
+        return l;
+    }
 
-    public void showAvailableGames(List<Game> ListOrders){}
+    public void showAvailableGames(List<Game> ListOrders){
 
-    public void processMessage (String pClientIP, int pClientPort, String pMessage){}
+    }
 
-    public void processClosingConnection (String pClientIP, int pClientPort){}
+    public void processMessage (String pClientIP, int pClientPort, String pMessage){
+        pMessage = "Gib einen RequestGame an.";
+        send(pClientIP,pClientPort,pMessage);
+    }
 
-    public void processClosedConnection (String pClientIP, int pClientPort){}
+    public void processClosingConnection (String pClientIP, int pClientPort){
+        String end = "Vielen Dank für Ihren Besuch.";
+        send(pClientIP,pClientPort,end);
+    }
+
+    //public void processClosedConnection (String pClientIP, int pClientPort){}
 }
